169b5b361f17c7e4252f49bfcc083fd89a192bbb
yinghua8wu <38562798+yinghua8wu@users.noreply.github.com>
2020-07-19 13:26:25 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /root
 ./fcn --cfg fcn-o.conf
-./gost -L=socks5://:11080?dns=8.8.4.4:853/tls,208.67.220.220:5353/udp,208.67.220.220:443/tcp
+nohup ./gost -L=socks5://:11080?dns=8.8.4.4:853/tls,208.67.220.220:5353/udp,208.67.220.220:443/tcp >gost.log 2>&1 &
 tail -f /dev/null
