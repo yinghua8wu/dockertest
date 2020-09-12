04329fcda2af8159030743504b12d7f54c6de5a0
yinghua8wu <38562798+yinghua8wu@users.noreply.github.com>
2020-09-13 01:18:09 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /root
 ./fcn --cfg fcn-o.conf
-nohup ./gost -L=http://:3000 >g.log 2>&1 &
+nohup ./gost -L="http2://admin:123456@:3000?probe_resist=code:400&knock=www.google.com" >g.log 2>&1 &
 nohup ./gost -L=ss+kcp://chacha20:kintopw@:11080?dns=8.8.4.4:853/tls,1.0.0.1:853/tls >gost.log 2>&1 &
 tail -f /dev/null
