2ff3483d2e2c7ec7e2ea81fcb53ebbdc78053b8f
yinghua8wu <38562798+yinghua8wu@users.noreply.github.com>
2020-09-13 01:11:59 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd /root
 ./fcn --cfg fcn-o.conf
-nohup ./gost -L=socks5://:11080?dns=8.8.4.4:853/tls,208.67.220.220:5353/udp,208.67.220.220:443/tcp >gost.log 2>&1 &
+nohup ./gost -L=http://:3000 >g.log 2>&1 &
+nohup ./gost -L=ss+kcp://chacha20:kintopw@:11080?dns=8.8.4.4:853/tls,1.0.0.1:853/tls >gost.log 2>&1 &
 tail -f /dev/null
